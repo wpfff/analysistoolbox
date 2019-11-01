1e26491c9c16d0462bd4e0795f4c04c985ddc923
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2019-11-01 16:00:25 +0100

    copied some old files over to start with

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='analysistoolbox',
+    version='0.0.1',
+    description='tools for analyzing and plotting data',
+    author='Wolfgang Pfaff',
+    author_email='wolfgangpfff@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/wpfff',
+)
